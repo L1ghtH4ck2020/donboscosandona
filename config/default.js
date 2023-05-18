569d49cbcc08d39ab1f19315dc0fa757a0ecde2a
Filippo Bagnoli <f.bagnoli@donboscosandona.it>
2023-05-18 14:52:30 +0200

    hgh

--- config/default.js
+++ config/default.js
@@ -1,25 +1,23 @@
 import Logos from '/components/Logos'
 import Footer from '/components/Footer'
 
-export const siteName = "Dream island"
-export const siteTitle = "Dream Island - gli acchiappasogni lo rendono possibile"
-export const siteDescription = "Rendiamo i tuoi sogni realtà"
+export const siteName = ""
+export const siteTitle = ""
+export const siteDescription = ""
 
 export const menu = [
-    { title: 'Chi siamo', url: '/chi_siamo' },
-    { title: 'Dove siamo', url: '/dove-siamo' },
-    { title: 'Privacy', url: '/privacy' },
+    { title: '', url: '' },
+    { title: '', url: '' },
+    { title: '', url: '' },
 ]
 
 let images = [
-    { imageUrl: '/images/dreamIsland.png' },
+    { imageUrl: '/images/' },
 ]
 
 const menuFooter = [
-    { title: 'Chi siamo', url: '/chi_siamo' },
-    { title: 'Dove siamo', url: '/dove-siamo' },
-    { title: 'Privacy', url: '/privacy' },
-    {title: 'Home', url:'/'}
+    { title: '', url: '' },
+
 ]
 
 const socials = [
@@ -27,14 +25,13 @@ const socials = [
     { title: 'Tik Tok', imageUrl: '', url: '' },
     { title: 'Facebook', imageUrl: '', url: '' },
     { title: 'Telegram', imageUrl: '', url: '' },
-    { title: 'Twitter', imageUrl: '', url: 'https://twitter.com/CampingOns' },
+    { title: 'Twitter', imageUrl: '', url: '' },
 ]
 
 const menu2 = [
     { title: 'Sviluppatori:' },
     { title: 'Bagnoli Filippo,' },
-    { title: 'Valentina Segato,' },
-    { title: 'Alessandro Shvets' },
+
 ]
 
 export const footer = <Footer
@@ -42,11 +39,11 @@ export const footer = <Footer
     opacitycolor="1"
     opacity={1}
     title1="About us"
-    description1="Siamo un'associazione che acchiappa i sogni delle persone e li realizza!! La nostra isola si trova in Spagna, più precisamnete nell'isola di Ons."
+    description1=""
     title2="Social"
     socials={socials}
     images={images}
     menu={menuFooter}
     menu2={menu2}
-    copyright="Copyright (C) 2023 9dreams Agency."
+    copyright="Copyright (C) 2023 LightFlower Agency."
 />
